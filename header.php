9a9a3e69e149459f49fc65389557aae6409c9f1f
Greg London <gregdlondon@gmail.com>
2016-06-11 08:40:52 -0500

    Added FB script for comments notifications.
    Closes #5

--- header.php
+++ header.php
@@ -24,6 +24,15 @@
 
 <body>
 
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=223751357754052";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
 <div id="wrap">
 
 	<div id="menu">
